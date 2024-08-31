2fd44a7d831b62600b5a86cc08608082b4cc32ad
MahdiTavakol <61837861+MahdiTavakol@users.noreply.github.com>
2024-08-31 10:37:05 +0100

    Add files via upload

---
+++ Ray_Tracing_6/rtweekend.h
@@ -0,0 +1,25 @@
+#ifndef RT_WEEKEND
+#define RT_WEEKEND
+
+
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <memory>
+
+using std::make_shared;
+using std::shared_ptr;
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+inline double degrees_to_radius(double degree)
+{
+	return degree * pi / 180.0;
+}
+
+#include "color.h"
+#include "ray.h"
+#include "vec3.h"
+
+#endif
