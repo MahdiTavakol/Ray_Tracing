98ba57184fdf6e65a1c88c7e92ff45c2dbd4f7be
MahdiTavakol <61837861+MahdiTavakol@users.noreply.github.com>
2024-09-03 20:02:11 +0100

    Update RayTracing_12.cpp

--- Ray_Tracing_19/RayTracing_12.cpp
+++ Ray_Tracing_19/RayTracing_12.cpp
@@ -1,77 +1,143 @@
-#ifndef RAYS_ARRAY_H
-#define RAYS_ARRAY_H
-
 #include <iostream>
+#include <fstream>
+
 #include <mpi.h>
 
-class rays_array
+
+#include "rtweekend.h"
+
+#include "camera.h"
+#include "hittable.h"
+#include "hittable_list.h"
+#include "material.h"
+#include "sphere.h"
+#include "write.h"
+#include "camera_parallel.h"
+
+
+
+
+
+int main()
 {
-public:
-	rays_array(const int& _num_pixels_x, const int& _num_pixels_y) : num_pixels_x(_num_pixels_x), num_pixels_y(_num_pixels_y)
-	{
-		allocate_array(rs, num_pixels_x, num_pixels_y);
-		allocate_array(gs, num_pixels_x, num_pixels_y);
-		allocate_array(bs, num_pixels_x, num_pixels_y);
-	}
-	rays_array(double* _rs, double* _gs, double* _bs, int _width, int _height) :num_pixels_x(_width), num_pixels_y(_height)
-	{
-		this->rs = (double**) malloc(num_pixels_x*sizeof(double*));
-		this->gs = (double**) malloc(num_pixels_x*sizeof(double*));
-		this->bs = (double**) malloc(num_pixels_x*sizeof(double*));
-		for (int i = 0; i < _width; i++)
+	// MPI
+	MPI_Init(NULL, NULL);
+
+	// World
+	hittable_list world;
+
+	auto ground_material = make_shared<lambertian>(color(0.5,0.5,0.5));
+	world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+
+	for (int a = -11; a < 11; a++)
+		for (int b = -11; b < 11; b++)
 		{
-			this->rs[i] = &_rs[i * _height];
-			this->gs[i] = &_gs[i * _height];
-			this->bs[i] = &_bs[i * _height];
+			auto choose_mat = random_double();
+			point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
+
+			if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+				shared_ptr<material> sphere_material;
+
+				if (choose_mat < 0.8) {
+					// diffuse
+					auto albedo = color::random() * color::random();
+					sphere_material = make_shared<lambertian>(albedo);
+					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+				}
+				else if (choose_mat < 0.95) {
+					// metal
+					auto albedo = color::random(0.5, 1);
+					auto fuzz = random_double(0, 0.5);
+					sphere_material = make_shared<metal>(albedo, fuzz);
+					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+				}
+				else {
+					// glass
+					sphere_material = make_shared<dielectric>(1.5);
+					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+				}
+			}
+
 		}
-	}
-	~rays_array()
-	{
-		deallocate_array(rs);
-		deallocate_array(gs);
-		deallocate_array(bs);
-	}
 
-	double** rs, ** gs, **bs;
-	int num_pixels_x, num_pixels_y;
+	auto material1 = make_shared<dielectric>(1.5);
+	world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
 
-private:
-	void allocate_array(double**& array, const int& num_x, const int& num_y)
-	{
-		double* temp = (double*)malloc(num_x * num_y * sizeof(double));
-		array = (double**)malloc(num_x * sizeof(double*));
+	auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+	world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
 
-		for (int i = 0; i < num_x; i++)
-			array[i] = &temp[num_y * i];
-	}
+	auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+	world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
 
-	void deallocate_array(double**& array)
-	{
-		free(array[0]);
-		free(array);
-		array = nullptr;
-	}
+
+
+	// Output file
+	std::ofstream file("RT20.ppm");
+
+
+	// Camera
+	camera_parallel cam;
+
+	cam.aspect_ratio = 16.0 / 9.0;
+	cam.image_width = 1920;
+	cam.file = &file;
+	cam.samples_per_pixel = 500;
+	cam.max_depth = 50;
+	cam.initialize();
+
+	// distribute load
+	int size, rank;
+	MPI_Comm MPI_world(MPI_COMM_WORLD);
+	MPI_Comm_rank(MPI_world, &rank);
+	MPI_Comm_size(MPI_world, &size);
+	int width_per_node = int(cam.image_width / size + 1);
+	int height_per_node = cam.image_height;
+	int width_min = rank * width_per_node;
+	int width_max = width_min + width_per_node;
+	int height_min = 0;
+	int height_max = cam.image_height;
+	cam.set_range(width_min, width_max, height_min, height_max);
+
+	// rays_array
+	rays_array r_array(width_per_node, height_per_node);
+
+	//cam.vfov = 90;
+	cam.vfov = 20;
+	cam.lookfrom = point3(13, 2, 3);
+	cam.lookat = point3(0, 0, 0);
+	cam.vup = point3(0, 1, 0);
+
+
+	cam.defocus_angle = 0.6;
+	cam.focus_dist = 10.0;
+
+	cam.render(world,r_array);
+
+
+	// gather the data
+	double* rs = r_array.rs[0];
+	double* gs = r_array.gs[0];
+	double* bs = r_array.bs[0];
 	
-};
+	
 
-void rays_array_add(rays_array& _rays_array, const int& i, const int& j, const color& _color)
-{
-	if (i >= _rays_array.num_pixels_x ||
-		j >= _rays_array.num_pixels_y ||
-		i < 0 ||
-		j < 0) 
-	{
-		int rank;
-		MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-		std::cout << "Out of boundary! from rank " << rank << " for " << i << "," << j << std::endl;
-		return;
+	double* rs_all = (double*)malloc(width_per_node * height_per_node * size * sizeof(double));
+	double* gs_all = (double*)malloc(width_per_node * height_per_node * size * sizeof(double));
+	double* bs_all = (double*)malloc(width_per_node * height_per_node * size * sizeof(double));
+	
+
+	//
+	MPI_Allgather(rs, width_per_node * height_per_node, MPI_DOUBLE, rs_all, width_per_node * height_per_node,MPI_DOUBLE, MPI_world);
+	MPI_Allgather(gs, width_per_node * height_per_node, MPI_DOUBLE, gs_all, width_per_node * height_per_node,MPI_DOUBLE, MPI_world);
+	MPI_Allgather(bs, width_per_node * height_per_node, MPI_DOUBLE, bs_all, width_per_node * height_per_node,MPI_DOUBLE, MPI_world);
+	
+	rays_array r_array_all(rs_all, gs_all, bs_all, cam.image_width, cam.image_height);
+
+	// write class
+	if (rank == 0) {
+		write write_obj(&file, &r_array_all, cam.image_width, cam.image_height);
+		write_obj.write_file();
 	}
-	_rays_array.rs[i][j] = _color.x();
-	_rays_array.gs[i][j] = _color.y();
-	_rays_array.bs[i][j] = _color.z();
+
+	MPI_Finalize();
 }
-
-
-
-
-#endif
