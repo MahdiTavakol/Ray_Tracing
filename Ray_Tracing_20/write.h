5d8d8b5591ba9fbba27e7b7ce4a3fc4bceca8711
MahdiTavakol <61837861+MahdiTavakol@users.noreply.github.com>
2024-09-03 21:40:27 +0100

    Update write.h

--- Ray_Tracing_20/write.h
+++ Ray_Tracing_20/write.h
@@ -6,16 +6,17 @@
 
 class write {
 public:
-	write(std::ofstream* _file, color_data* _colors, int _image_width, int _image_height): file(_file), colors(_colors), image_width(_image_width), image_height(_image_height) {}
+	write(std::ofstream* _file, color_array* _colors, int _image_width, int _image_height): file(_file), colors(_colors), image_width(_image_width), image_height(_image_height) {}
 	void write_file()
 	{
 		*file << "P3\n" << image_width << " " << image_height << "\n255\n";
+		color_data** c_data = *colors.return_array();
 
 		for (int j = 0; j < image_height; j++)
 		{
 			for (int i = 0; i < image_width; i++)
 			{
-				*file << colors[i * image_width + j];
+				*file << c_data[i][j];
 			}
 		}
 	}
