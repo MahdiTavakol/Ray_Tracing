82a7f9588b2e0807ce30e8d95c3acfbc2f739d8b
Mahdi Tavakol <61837861+MahdiTavakol@users.noreply.github.com>
2024-09-14 12:26:41 +0100

    Add files via upload

---
+++ Ray_Tracing_21/camera_parallel.h
@@ -0,0 +1,45 @@
+#ifndef CAMERA_PARALLEL_H
+#define CAMERA_PARALLEL_H
+
+#include "camera.h"
+
+class camera_parallel : public camera
+{
+public:
+	camera_parallel(const int _width_min, const int _width_max,const int _height_min, const int _height_max) :
+		width_min(_width_min), width_max(_width_max), height_min(_height_min), height_max(_height_max) {}
+	camera_parallel(): width_min(0), width_max(0), height_min(0), height_max(0) {}
+	void render(const hittable& world, color_array& c_a)
+	{
+		initialize();
+		for (int j = height_min; j < height_max; j++)
+		{
+			for (int i = width_min; i < width_max; i++)
+			{
+				color pixel_color(0, 0, 0);
+				for (int sample = 0; sample < samples_per_pixel; sample++)
+				{
+					ray r = get_ray(i, j);
+					pixel_color += ray_color(r, max_depth, world);
+				}
+				color_data** c_data = c_a.return_array();
+				pixel_color = pixel_samples_scale * pixel_color;
+				c_data[i - width_min][j - height_min].r = pixel_color.x();
+				c_data[i - width_min][j - height_min].g = pixel_color.y();
+				c_data[i - width_min][j - height_min].b = pixel_color.z();	
+			}
+		}
+	}
+	void set_range(const int& _width_min, const int& _width_max, const int& _height_min, const int& _height_max)
+	{
+		this->width_min = _width_min;
+		this->width_max = _width_max;
+		this->height_min = _height_min;
+		this->height_max = _height_max;
+	}
+
+private:
+	int height_min, height_max, width_min, width_max;
+};
+
+#endif
