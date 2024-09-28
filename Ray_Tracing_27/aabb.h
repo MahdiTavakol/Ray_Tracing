d429da16b4b155a7d6a7c21c049116404c1b3ac2
Mahdi Tavakol <61837861+MahdiTavakol@users.noreply.github.com>
2024-09-28 13:54:09 +0100

    Add files via upload

---
+++ Ray_Tracing_27/aabb.h
@@ -0,0 +1,33 @@
+#ifndef AABB_H
+#define AABB_H
+
+#include "rtweekend.h"
+#include "interval.h"
+#include "vec3.h"
+#include "ray.h"
+
+class aabb {
+public:
+	interval x, y, z;
+
+	aabb() {}
+
+	aabb(const interval& _x, const interval& _y, const interval& _z);
+
+	aabb(const point3& a, const point3& b);
+
+	aabb(const aabb& box0, const aabb& box1);
+
+	const interval& axis_interval(int n) const;
+
+	bool hit(const ray& r, interval ray_t) const;
+
+	int longest_axis() const;
+	
+	static const aabb empty, universe;
+	
+private:
+	void pad_to_minimums();
+};
+
+#endif
